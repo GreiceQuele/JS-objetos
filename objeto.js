5f407830f7835d5bb07e9f3336c1ad4508f3b7e5
Quele <greice.caetano@gmail.com>
2024-03-27 15:24:22 -0300

    Criado novo arquivo e colocando notação por chaves

---
+++ objeto.js
@@ -0,0 +1,13 @@
+const listaCPFs = ['12312312312' , '34534534534'];
+
+const infoPessoa = ['nome', 'José','idade:', 33];
+
+console.log(infoPessoa);
+
+const objPessoa = {
+    nome: 'José da Silva',
+    idade: 32 ,
+    cpf: '12312312312',
+    turma: 'Javascript'
+}
+
